07ca756e8f6f8389e1f55484f396d2ba6837f8d7
KunalTanwar130 <kunaldropshipping13@gmail.com>
2025-10-30 14:55:14 +0530

    Create AppConfig.java

---
+++ Part A/AppConfig.java
@@ -0,0 +1,9 @@
+package com.springdi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.springdi")
+public class AppConfig {
+}

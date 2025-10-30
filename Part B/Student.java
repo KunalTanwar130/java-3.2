cb9fe0c83b9fd25a864a4aded12d092baa432734
KunalTanwar130 <kunaldropshipping13@gmail.com>
2025-10-30 14:56:34 +0530

    Create Student.java

---
+++ Part B/Student.java
@@ -0,0 +1,28 @@
+package com.student.hibernate.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String course;
+    private double marks;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCourse() { return course; }
+    public void setCourse(String course) { this.course = course; }
+
+    public double getMarks() { return marks; }
+    public void setMarks(double marks) { this.marks = marks; }
+}
